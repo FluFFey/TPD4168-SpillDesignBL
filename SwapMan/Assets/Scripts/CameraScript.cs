3129f45bd002b2f3b2288f0fa4c3810e698f8e7a
Henning Luick <henningel@gmail.com>
2017-10-04 20:44:58 +0200

    fixed some stuff (ghost swap+smooth camera etc)

--- SwapMan/Assets/Scripts/CameraScript.cs
+++ SwapMan/Assets/Scripts/CameraScript.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class CameraScript : MonoBehaviour {
 
     public GameObject target; //target to follow
+    public Vector2 targetOffset;
 	// Use this for initialization
 	void Start () {
-		
+        targetOffset = new Vector3(0, 0, 0);
 	}
 	// Update is called once per frame
-	void Update () {
-        transform.position = target.transform.position;
-        transform.Translate(new Vector3(0, 0, -5));
+	void LateUpdate () {
+        float followSpeed = 1.0f;
+        Vector3 distanceVector = target.transform.position+(new Vector3(targetOffset.x, targetOffset.y,0)) - transform.position;
+        float distanceFromTarget = distanceVector.magnitude;
+        transform.Translate(new Vector3(distanceVector.x, distanceVector.y, 0)*distanceFromTarget*followSpeed*Time.deltaTime);
+        targetOffset = new Vector3(0, 0, 0);
 	}
 }
